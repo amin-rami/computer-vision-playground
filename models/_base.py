d3336aed0b688394a04d2719c0a00f489f6a3ada
Mohammad Amin Rami <ma.rami@yahoo.com>
2024-09-24 13:57:15 +0330

    add infer method for BaseModule

--- models/_base.py
+++ models/_base.py
@@ -1,7 +1,14 @@
+import torch
 import torch.nn as nn
 
 
 class BaseModule(nn.Module):
-    def __init__(self, name: str = None) -> None:
+    def __init__(self, name: str = None):
         super().__init__()
         self.name = name if name else f"{type(self).__name__}{id(self)}"
+
+    def infer(self, x: torch.Tensor) -> torch.Tensor:
+        self.eval()
+        with torch.no_grad():
+            x = self.forward(x)
+        return x
