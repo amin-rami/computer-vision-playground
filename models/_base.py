93d4d5cf88d3c4f319761fb7eb9f68759253e1b1
Mohammad Amin Rami <ma.rami@yahoo.com>
2024-09-24 13:21:09 +0330

    flake8

--- models/_base.py
+++ models/_base.py
@@ -2,6 +2,6 @@ import torch.nn as nn
 
 
 class BaseModule(nn.Module):
-    def __init__(self, name: str=None) -> None:
+    def __init__(self, name: str = None) -> None:
         super().__init__()
         self.name = name if name else f"{type(self).__name__}{id(self)}"
